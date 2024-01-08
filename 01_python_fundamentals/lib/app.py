2e9e6dfd1b9c45f5c007212e583251e5e86734b3
tyler kim <yoonsoo.kim11@gmail.com>
2024-01-08 14:17:30 -0500

    completed lecture q

--- 01_python_fundamentals/lib/app.py
+++ 01_python_fundamentals/lib/app.py
@@ -6,7 +6,7 @@
 	# Python datatypes 
 
 # 🚨 To enable ipdb debugging, first import "ipdb"
-import ipdb
+# import ipdb
 
 # 1. ✅ Create a condition to check a pet's mood
     # If "pet_mood" is "Hungry!", "Rose needs to be fed."
@@ -15,21 +15,66 @@ import ipdb
 
     # Note => Feel free to set your own values for "pet_mood" to view various outputs.
 
-pet_mood = "Hungry!"
-pet_name = "Rose"
+# pet_mood = "Hungry!"
+# pet_name = "Rose"
+#1. solution
+# if pet_mood == "Hungry!":
+#     print(f'{pet_name} needs to be fed.')
+# elif pet_mood == 'Rowdy!':
+#     print(f'{pet_name} needs a walk.')
+# else:
+#     print(f"{pet_name} is all good.")
+
+# time = 'dinner'
+# if time == 'morning':
+#     print('eat breakfast')
+# elif time == 'afternoon':
+#     print('eat lunch')
+# elif time == 'dinner':
+#     print('eat dinner')
+# else:
+#     print('go to sleep')
 
 # 2. ✅ Create a ternary operator using "pet_mood" as a condition:
     # If pet_food is "Hungry!" => "Rose needs to be fed."
     # In all other cases => "Rose is all good."
+# pet_mood = "Hungry!"
+# pet_name = "Rose"
+# #2. solution
+# print(f"{pet_name} needs to be fed") if pet_mood == 'Hungry!' else print(f"{pet_name} is all good.")
+# time = 'dinner'
+# print('eat breakfast') if time == 'morning' else print('study python')
+
 
 # 3. ✅ Create a function (say_hello) that returns the string "Hello, world!"
     # Test invocation of "say_hello" in ipdb using "say_hello()"
     # say_hello() => "Hello, world!"
 
+#3. solution
+# def say_hello():
+#     print('hello world')
+# say_hello()
+
+
 # 4. ✅ Create a function (pet_greeting) that will return a string with interpolated pet's name
     # Test invocation of "pet_greeting" in ipdb using "pet_greeting()"
     # pet_greeting("Rose") => "Rose says hello!"
     # pet_greeting("Spot") => "Spot says hello!"
+
+#4. solution
+# def pet_greeting(name):
+#     print(f"{name} says hello!")
+# pet_greeting('Rose')
+
+# # can do math operation inside f 
+# print(f'{2*3}')
+
+# def schedule(time):
+#     print(f"It's {time}, let's eat breakfast.")
+# schedule('morning')
+
+
+
 
 # 5. ✅ Move conditional logic from Deliverable 1 into a function (pet_status) so that we may use it with different pets / moods
     # Test invocation of "pet_status" in ipdb using "pet_status(pet_name, pet_mood)"
@@ -37,8 +82,19 @@ pet_name = "Rose"
     # pet_greeting("Spot", "Rowdy!") => "Spot needs a walk."
     # pet_greeting("Bud", "Relaxed") => "Bud is all good."
     
+
     # Take a moment to note that "pet_name" and "pet_mood" parameters are within Local Scope and take priority over "pet_name" and "pet_mood"
     # in Global Scope.
+#5 solution
+def pet_status(pet_name, pet_mood):
+    if pet_mood == "Hungry!":
+        print(f'{pet_name} needs to be fed.')
+    elif pet_mood == 'Rowdy!':
+        print(f'{pet_name} needs a walk.')
+    else:
+        print(f"{pet_name} is all good.")
+pet_status("Tyler", 'Rowdy!')
+
 
 # 6. ✅ Create a function (pet_birthday) that will increment a pet's age up by 1. Use try / except to handle errors. 
     # If our function is given an incorrect datatype, it should handle the TypeError exception and alert the user
@@ -46,6 +102,15 @@ pet_name = "Rose"
     # pet_birthday("oops") => "Type Error Occurred"
 
     # Note => To view more common Python exceptions, visit https://docs.python.org/3/library/exceptions.html
+
+
+#6 solution
+def pet_birthday(age):
+    try:
+        print(f"Happy Birthday! Your pet is now {age + 1}.")
+    except TypeError:
+        print("Type Error Occured")
+pet_birthday('oops')
 
 # 🚨 To create an ipdb breakpoint, comment / uncomment line below:
 # ipdb.set_trace()
