9ecc0df2d82d14c4c4f0540ff9028e99e228bf69
tyler kim <yoonsoo.kim11@gmail.com>
2024-01-10 11:18:16 -0500

    lecture f/a

--- 03_oop/lib/pet.py
+++ 03_oop/lib/pet.py
@@ -4,10 +4,39 @@
 # 2. ✅ Instantiate a few pet instance 
     # Compare the pet instances to demonstrate they are not the same object
     # Note: add 'pass' to the pet class 
-
-
+import ipdb
 class Pet:
-    # pass
+    def __init__(self, name, age, breed, temperament):
+
+        #4 instance attributes
+        self.name = name 
+        self.age = age
+        self.breed = breed
+        self.temperament = temperament
+    def print_details(self):
+        print(f'''
+              name: {self.name},
+              age: {self.age},
+              breed: {self.breed},
+              temperament: {self.temperament}
+              ''')
+tyler = Pet('tyler', 31, 'beagle', 'sassy') #tyler.name = 'Tyler Kim' - can use dot.notataion to access attributes or change it
+(tyler.print_details())
+# ipdb.set_trace()
+
+jindo = Pet('jindo', 3, 'jindo dog', 'happy')
+# ipdb.set_trace()# breakpoint - now we can enter ipdb in terminal and try out different things
+class Flower:
+    def __init__(self, name, color, region):
+        self.name = name
+        self.color = color
+        self.region = region
+
+tulip = Flower('Tulip', 'red', 'Europe')
+sunflower = Flower('Sunflower', 'yellow', 'South America')
+rose = Flower('Rose', 'yellow', 'Asia')
+# ipdb.set_trace()
+
 # 3. ✅ Demonstrate __init__ 
     # Add arguments to instances  
     # use dot notation to access their attributes 
